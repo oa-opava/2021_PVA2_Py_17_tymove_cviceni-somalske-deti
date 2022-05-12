c65e83e10a2c4200322153aa30a503407643d85f
Marek Hegr <mhegr04@gmail.com>
2022-05-12 08:39:14 +0200

    sprint 3

--- .github/我討厭阿拉伯人.py
+++ .github/我討厭阿拉伯人.py
@@ -1,6 +1,11 @@
 import re
+
 regex = r"[0-9]{6}\/[0-9]{4}"
 regex2 = r"[0-9]{2}[5]{1}[0-9]{3}\/[0-9]{4}"
+
+print("Vložte rodné číslo:")
+rodnecislo = input()
+
 class RC_marek():
     def __init__(self, rodnecislo):
         self.rodnecislo = rodnecislo
@@ -15,5 +20,10 @@ class RC_marek():
             return False
 
     def pohlavi(self):
-        if re.fullmatch(regex, self.rodnecislo):
+        if re.fullmatch(regex2, self.rodnecislo):
+            return "Z"
+        else:
+            return "M"
 
+
+RC_marek.pohlavi(47901/695)

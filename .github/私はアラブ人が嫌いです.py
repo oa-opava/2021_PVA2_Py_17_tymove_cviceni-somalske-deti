f1dbacaf5e6c1ed49cb1c6acfe8813a14f276888
Filip Šafarčík <safocz@seznam.cz>
2022-05-12 08:02:20 +0200

    ohuafdfshashlidasjidasjisajiijasodjoasio

---
+++ .github/私はアラブ人が嫌いです.py
@@ -0,0 +1,20 @@
+#class Rocislo:
+ #   def __init__(self, rodnecislo):
+  #      self.rodnecislo = rodnecislo
+#
+ #   def jeSpravne(self):
+   #     if self.rodnecislo ==
+
+from datetime import date
+
+
+
+def datumNarození(self,Dnarozeni):
+    self.Dnarozeni = Dnarozeni
+    self.Dnarozeni = date
+    date = ('1999-12-04')
+    return date
+
+
+
+
